025ac32a9aa156aa83dbc5a19b1cc04da23e1e96
Sérgio Freitas <sjcsgfreitas@hotmail.com>
2026-09-24 11:28:55 +0200

    Add config draft.

--- discord/assistant-api/src/game/forever/config.ts
+++ discord/assistant-api/src/game/forever/config.ts
@@ -1,20 +1,54 @@
 export const FOREVER_CONFIG = {
-    gameVersion: 'Forever' as const,
-    factions: {
-        ["Alliance"]: {
-            races: [
-                'Dwarf',
-                'Gnome',
-                'Human',
-                'Night Elf',
-            ] as const,
+  gameVersion: 'Forever' as const,
+  factions: {
+    ['Alliance']: {
+      races: {
+        ['Dwarf']: {
+          classes: ['Hunter', 'Paladin', 'Priest', 'Rogue', 'Warrior', 'Shaman'] as const,
         },
-        ["Horde"]: {
-            races: [
-                'Troll',
-                'Orc',
-                'Tauren',
-                'Undead',
-            ] as const,
-        }, 
-    };
+        ['Gnome']: {
+          classes: ['Mage', 'Rogue', 'Warlock'] as const,
+        },
+        ['Human']: {
+          classes: ['Paladin', 'Priest', 'Rogue', 'Warrior'] as const,
+        },
+        ['Night Elf']: {
+          classes: ['Druid', 'Hunter', 'Priest'] as const,
+        },
+        ['Skyborn']: {
+          classes: ['Druid', 'Mage', 'Paladin', 'Priest', 'Rogue', 'Warrior'] as const,
+        },
+      },
+      ['Horde']: {
+        races: {
+          ['Troll']: {
+            classes: ['Hunter', 'Rogue', 'Shaman', 'Warrior', 'Mage', 'Warlock'] as const,
+          },
+          ['Orc']: {
+            classes: ['Hunter', 'Rogue', 'Warlock', 'Shaman', 'Warrior'] as const,
+          },
+          ['Tauren']: {
+            classes: ['Druid', 'Hunter', 'Priest', 'Shaman'] as const,
+          },
+          ['Undead']: {
+            classes: ['Paladin', 'Warrior', 'Mage', 'Priest', 'Rogue', 'Warlock'] as const,
+          },
+          ['Skyborn']: {
+            classes: ['Druid', 'Priest', 'Rogue', 'Shaman', 'Warrior'] as const,
+          },
+        },
+      },
+    },
+    classes: {
+      ['Druid']: {
+        specializations: {
+          ['Balance']: {
+            roles: ['Ranged DPS'],
+            raidBuffs: ['Ranged DPS', 'Healers'],
+            groupBuffs: ['Ranged DPS'],
+          },
+        },
+      },
+    },
+  },
+};
